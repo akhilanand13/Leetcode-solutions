74291c506f8ed09a518b9ac602321519409bb055
akhilanand13 <147290597+akhilanand13@users.noreply.github.com>
2025-07-24 17:36:26 +0530

    Add files via upload

---
+++ 1-two-sum.java
@@ -0,0 +1,17 @@
+//https://leetcode.com/problems/two-sum/
+//Time Complexity: O(n)
+//Space Complexity: O(n)
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> hashmap = new HashMap<Integer, Integer>();
+        for(int i = 0; i < nums.length; i++){
+            int remaining = target - nums[i];
+            if(hashmap.containsKey(remaining)){
+                return new int[] {hashmap.get(remaining), i};
+            }
+            hashmap.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+}
