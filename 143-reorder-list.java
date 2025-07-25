eb7ec0e6498058b852f34438007d575b9b8d0d3e
Akhil Anand Balu <147290597+akhilanand13@users.noreply.github.com>
2025-07-25 12:06:30 +0530

    Add files via upload

---
+++ 143-reorder-list.java
@@ -0,0 +1,34 @@
+//https://leetcode.com/problems/reorder-list/
+//Time Complexity: O(n)
+//Space Complexity: O(1)
+
+class Solution {
+    public void reorderList(ListNode head) {
+        if (head == null || head.next == null) return; 
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        ListNode secondHalf = reverseList(slow.next);
+        slow.next = null; 
+        ListNode firstHalf = head;
+        while (secondHalf != null) {
+            ListNode temp1 = firstHalf.next, temp2 = secondHalf.next;
+            firstHalf.next = secondHalf;
+            secondHalf.next = temp1;
+            firstHalf = temp1;
+            secondHalf = temp2;
+        }
+    }
+    private ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        while (head != null) {
+            ListNode nextNode = head.next;
+            head.next = prev;
+            prev = head;
+            head = nextNode;
+        }
+        return prev;
+    }
+}
