41715c844aeb4b57df2bb9009a4fba0ea5973252
Akhil Anand Balu <147290597+akhilanand13@users.noreply.github.com>
2025-07-27 21:20:50 +0530

    Add files via upload

---
+++ 42-trapping-rain-water.java
@@ -0,0 +1,31 @@
+//https://leetcode.com/problems/trapping-rain-water/
+//Time Complexity: O(n)
+//Space Complexity: O(1)
+
+class Solution {
+    public int trap(int[] height) {
+        if (height == null || height.length == 0) {
+            return 0;
+        }
+        int n = height.length;
+        int totalWater = 0, left = 0, right = n - 1, leftMax = 0, rightMax = 0;
+        while (left <= right) {
+            if (height[left] <= height[right]) {
+                if (height[left] >= leftMax) {
+                    leftMax = height[left];
+                } else {
+                    totalWater += leftMax - height[left];
+                }
+                left++;
+            } else {
+                if (height[right] >= rightMax) {
+                    rightMax = height[right];
+                } else {
+                    totalWater += rightMax - height[right];
+                }
+                right--;
+            }
+        }
+        return totalWater;
+    }
+}
