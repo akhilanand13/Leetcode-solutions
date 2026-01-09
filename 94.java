1aa7703b5c00c662b9cd5600b77da3920b842050
Akhil Anand Balu <147290597+akhilanand13@users.noreply.github.com>
2026-01-09 09:22:42 +0530

    Add files via upload

---
+++ 94.java
@@ -0,0 +1,20 @@
+//code.com/problems/binary-tree-inorder-traversal/description/
+//Time Complexity: O(n)
+//Space Complexity: O(n)
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode current = root;
+        while (current != null || !stack.isEmpty()) {
+            while (current != null) {
+                stack.push(current);
+                current = current.left;
+            }
+            current = stack.pop();
+            result.add(current.val);
+            current = current.right;
+        }
+        return result;
+    }
+}
