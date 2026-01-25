427acb44017795b62b1dce4d42b4fa538827166b
Akhil Anand Balu <147290597+akhilanand13@users.noreply.github.com>
2026-01-25 07:51:47 +0530

    Solution for 1984

---
+++ 1984.py
@@ -0,0 +1,14 @@
+"""
+https://leetcode.com/problems/minimum-difference-between-highest-and-lowest-of-k-scores/description/
+Time Complexity: O(nlogn)
+Space Complexity: O(n)
+"""
+class Solution:
+    def minimumDifference(self, nums: List[int], k: int) -> int:
+        nums.sort()
+        l, r = 0, k - 1
+        res = float("inf")
+        while r < len(nums):
+            res = min(res, nums[r] - nums[l])
+            l, r = l+1, r+1
+        return res
