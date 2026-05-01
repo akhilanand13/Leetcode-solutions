0de595e3a253bb7471a9c5ea9fd976fececaf69d
Akhil <akhilanandbalu2004@gmail.com>
2026-05-01 21:02:05 +0530

    valid perfect square

---
+++ 367.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isPerfectSquare(self, num: int) -> bool:
+    # Time Complexity O(sqrt(n)) Brute Force Approach
+        # for i in range(1, num+1):
+        #     if i * i == num:
+        #         return True
+        #     if i * i > num:
+        #         return False
+    # Time Complexity O(log(n)) Binary Search Approach
+        l, r = 1, num
+        while l <= r:
+            mid = (l + r) // 2
+            if mid * mid > num:
+                r = mid -1
+            elif mid * mid < num:
+                l = mid + 1
+            else:
+                return True
+        return False
